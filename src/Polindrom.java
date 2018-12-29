153c2ca06ee9ffc3d905e421f421c98cde8b1eac
Catalina <cristea.catalina89@gmail.com>
2018-12-29 15:34:35 +0200

    Initial commit

---
+++ src/Polindrom.java
@@ -0,0 +1,22 @@
+public class Polindrom {
+
+    public static void main(String[] args) {
+
+        String cuvant = "reper";
+        String cuvantIntors = "reper";
+
+        if ( cuvant.equals(cuvantIntors)){
+
+            System.out.println("Cuvantul este palindrom");
+        }else {
+            System.out.println("Cuvantul nu este palindrom");
+        }
+
+
+
+
+    }
+
+
+}
+
